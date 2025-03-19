999ad472f7680ff05e07bac4d72a080fc952d421
Michele Perlotto <mick0974@outlook.it>
2025-03-19 14:47:12 +0100

    Implementazione modalità di gioco partita singola (issue #81)
    Refactoring e unificazione dei servizi:
    	- Unificata la generazione dei robot EvoSuite (T8) e Randoop (T9) nel nuovo servizio T0, accessibile tramite `generate.sh`.
    	- Riscritto il servizio di calcolo delle statistiche di copertura EvoSuite (T8) in Spring per migliorarne la manutenibilità e la resilienza.
    	- Corretto il salvataggio dei robot e la generazione delle statistiche di copertura in T1 per supportare il nuovo T8.
    
    Aggiornamenti ai gateway:
    	- Aggiornate le route nei gateway UI e API.
    	- Il gateway UI ora serve anche le immagini degli achievement.
    
    Sistema di esperienza e progressi utente:
    	- Esteso T4 con tabelle e controller per tracciare progressi, achievement e punti esperienza.
    	- Aggiornato T2 per creare l’istanza dei punti esperienza in T4 alla registrazione di un nuovo utente.
    	- Punti esperienza assegnati solo se l’utente non ha già riscattato quelli relativi a quel robot robot con la stessa difficoltà nella stessa classe sotto test e modalità di gioco.
    
    Gestione Sessione:
    	- La sessione ora mantiene:
    	  - Il codice utente e il risultato dell’ultima compilazione, evitando ricompilazioni inutili.
    	  - I parametri generali della partita (robot, difficoltà, modalità, codice di test, ecc.).
    	  - I parametri specifici della modalità scelta (es. tempo rimanente per "PartitaSingola").
    	- Salvataggio sessione esteso ai seguenti casi:
    	  - Chiusura della scheda o ritorno alla pagina precedente.
    	  - Scadenza del timer in "PartitaSingola", con opzione per valutare l’ultima compilazione o compilarne una finale prima della valutazione.
    
    Modalità di gioco "Partita Singola":
    	- Implementata modalità di gioco "Partita Singola"
    
    Pulizia codice e gestione legacy
    	- Spostati i servizi non più in uso (T8, T9 e la vecchia versione di T8) nella cartella `LegacyCode`.

--- selective_build_and_deploy.bat
+++ selective_build_and_deploy.bat
@@ -23,9 +23,10 @@ echo 3 - T4-G18
 echo 4 - T5-G2
 echo 5 - T7-G31
 echo 6 - T8-G21
-echo 7 - T9-G19
 echo 8 - ui_gateway
 echo 9 - api_gateway
+echo 9 - T0
+echo Select:
 set /p SELECTION=Scelte (es. 0 1 2 o 'all'):
 
 :: Se l'utente ha scelto "all", builda tutto
@@ -96,7 +97,7 @@ for %%i in (%SELECTION%) do (
         cd /d "%ROOT_DIR%"
     ) else if %%i==6 (
         echo Building T8-G21
-        cd /d "%ROOT_DIR%\T8-G21\Progetto_SAD_GRUPPO21_TASK8\Progetto_def\opt_livelli\Prototipo2.0\Serv"
+        cd /d "%ROOT_DIR%\T8-G21\T8"
         call mvn clean package || (echo Error in T8-G21 build & exit /b 1)
         cd /d "%ROOT_DIR%"
         docker build -t mick0974/a13:t8-g21 .
@@ -107,17 +108,6 @@ for %%i in (%SELECTION%) do (
         )
         cd /d "%ROOT_DIR%"
     ) else if %%i==7 (
-        echo Building T9-G19
-        cd /d "%ROOT_DIR%\T9-G19\Progetto-SAD-G19-master"
-        call mvn clean package || (echo Error in T9-G19 build & exit /b 1)
-        docker build -t mick0974/a13:t9-g19 .
-        docker compose up -d
-        if %ERRORLEVEL% neq 0 (
-            echo Error deploying T1-G11
-            exit /b 1
-        )
-        cd /d "%ROOT_DIR%"
-    ) else if %%i==8 (
         echo Building ui_gateway
         cd /d "%ROOT_DIR%\ui_gateway"
         docker build -t mick0974/a13:ui-gateway .
@@ -127,12 +117,11 @@ for %%i in (%SELECTION%) do (
             exit /b 1
         )
         cd /d "%ROOT_DIR%"
-    ) else if %%i==9 (
+    ) else if %%i==8 (
         echo Building api_gateway
         cd /d "%ROOT_DIR%\api_gateway"
         call mvn clean package || (echo Error in api_gateway build & exit /b 1)
         docker build -t mick0974/a13:api-gateway .
-        call mvn clean package
         if ERRORLEVEL 1 (
             echo Error in T8-G21 build during call mvn clean package
             exit /b 1
@@ -141,6 +130,22 @@ for %%i in (%SELECTION%) do (
         if %ERRORLEVEL% neq 0 (
             echo Error deploying T1-G11
             exit /b 1
+        )
+        cd /d "%ROOT_DIR%"
+    ) else if %%i==9 (
+        echo Building T0
+        cd /d "%ROOT_DIR%\T0\RandoopGenerator"
+        call mvn clean package || (echo Error in T0 build & exit /b 1)
+        docker build -t mick0974/a13:t0 .
+        if ERRORLEVEL 1 (
+          echo Error in T8-G21 build during call mvn clean package
+          exit /b 1
+        )
+        cd /d "%ROOT_DIR%\T0
+        docker compose up -d
+        if %ERRORLEVEL% neq 0 (
+          echo Error deploying T1-G11
+          exit /b 1
         )
         cd /d "%ROOT_DIR%"
     ) else (
